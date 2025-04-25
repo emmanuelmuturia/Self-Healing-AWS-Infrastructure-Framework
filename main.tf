eede81639db375d3fa78fc2dae1e6a3019d56c54
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-04-25 17:58:20 +0300

    Modify the Terraform configuration...

--- main.tf
+++ main.tf
@@ -143,3 +143,9 @@ resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.public_a.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
